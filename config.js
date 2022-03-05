c6311f098c204347bd7bbe4cb9cd9a50930a9106
17luffy <101005807+17luffy@users.noreply.github.com>
2022-03-05 21:17:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "8-3 vui vẻ nhé ",
-    introTitle: 'Thảo ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đườngkjjh
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thươncdg`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    introTitle: 'Tất Cả Các Bạn Nữ ',
+    introDesc: `Sắp 8/3 rồi hihi
+    đạt chúc ny tương lai hiu hiu 
+    8/3 thật nà zui zẻ,
+    btnIntro: 'Hề Hề',
+    title: 'Các bạn nữ có thích không ạ  🥰',
+    desc: 'không thích cũng phải thích ',
+    btnYes: 'Phải thích <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Tại sao lại chọn thích hiu hiu <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: ' mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
